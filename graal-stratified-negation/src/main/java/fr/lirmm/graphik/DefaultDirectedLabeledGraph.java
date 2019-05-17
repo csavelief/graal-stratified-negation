51b689c0e89031decb1b2e33150979ae418b1c48
Cyrille Savelief <csavelief@mncc.fr>
2019-05-17 14:50:24 +0200

    Remove dead code

--- graal-stratified-negation/src/main/java/fr/lirmm/graphik/DefaultDirectedLabeledGraph.java
+++ graal-stratified-negation/src/main/java/fr/lirmm/graphik/DefaultDirectedLabeledGraph.java
@@ -6,30 +6,18 @@ import java.util.Iterator;
 import fr.lirmm.graphik.graal.api.core.Rule;
 
 @Deprecated
-public class DefaultDirectedLabeledGraph<V,E> implements DirectedLabeledGraph{
+public class DefaultDirectedLabeledGraph<V,E> {
 
 	private final ArrayList<ArrayList<Pair<Integer, Character>>> adjacencyList;
-	private int nbVertices;
-	
+
 	public DefaultDirectedLabeledGraph(Iterable<Rule> rules) {
-		
 		this.adjacencyList = new ArrayList<>();
-		this.nbVertices = 0;
-		
 		for(Iterator<Rule> i = rules.iterator() ; i.hasNext() ; i.next())
 		{
 			this.adjacencyList.add(new ArrayList<>());
-			this.nbVertices++;
 		}
 	}
 
-	public void addEdge(int tail, int head, char label) {
-		ArrayList<Pair<Integer, Character>> l = adjacencyList.get(tail);
-		l.add(new Pair<>(head, label));
-		
-		adjacencyList.set(tail, l);
-	}
-	
 	public String toString()
 	{
 		StringBuilder s = new StringBuilder();
--- graal-stratified-negation/src/main/java/fr/lirmm/graphik/DirectedLabeledGraph.java
+++
@@ -1,9 +0,0 @@
-package fr.lirmm.graphik;
-
-import java.util.ArrayList;
-
-@Deprecated
-public interface DirectedLabeledGraph {
-
-	void addEdge(int tail, int head, char label);
-}
