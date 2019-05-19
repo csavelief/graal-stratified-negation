e2955c2892432bfe09e170462d1b3e608588896a
Cyrille Savelief <csavelief@mncc.fr>
2019-05-19 19:43:04 +0200

    Cleanup utils.

--- graal-stratified-negation/src/main/java/fr/lirmm/graphik/Utils.java
+++ graal-stratified-negation/src/main/java/fr/lirmm/graphik/Utils.java
@@ -3,8 +3,8 @@ package fr.lirmm.graphik;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import com.google.common.base.Throwables;
 import com.google.errorprone.annotations.Var;
 import fr.lirmm.graphik.graal.api.core.Atom;
 import fr.lirmm.graphik.graal.api.core.AtomSet;
@@ -22,139 +22,127 @@ class Utils {
 
   public static KBBuilder readKB(KBBuilder kbb, String fileRules, String fileFacts) {
 
-    /* Parsing Rules */
+    // Parsing Rules
     if (fileRules != null) {
+
       System.out.println("Rules : parsing of '" + fileRules + "'");
-      try {
-        InputStream ips = new FileInputStream(fileRules);
-        InputStreamReader ipsr = new InputStreamReader(ips, UTF_8);
-        BufferedReader br = new BufferedReader(ipsr);
+
+      try (BufferedReader br =
+          new BufferedReader(new InputStreamReader(new FileInputStream(fileRules), UTF_8))) {
+
         @Var
-        String ligne;
+        String row;
 
-        while ((ligne = br.readLine()) != null) {
-          if (ligne.charAt(0) != '%')
-            kbb.add(DlgpParserNeg.parseRule(ligne));
+        while ((row = br.readLine()) != null) {
+          if (row.charAt(0) != '%') {
+            kbb.add(DlgpParserNeg.parseRule(row));
+          }
         }
-
-        br.close();
-        ipsr.close();
-        ips.close();
-
       } catch (Exception e) {
-        System.out.println("Caca" + e.toString());
-        e.printStackTrace();
+        Throwables.getRootCause(e).printStackTrace();
       }
     }
 
-    /* Parsing Facts */
-
+    // Parsing Facts
     if (fileFacts != null) {
+
       System.out.println("Facts : parsing of '" + fileFacts + "'");
-      try {
-        InputStream ips = new FileInputStream(fileFacts);
-        InputStreamReader ipsr = new InputStreamReader(ips, UTF_8);
-        BufferedReader br = new BufferedReader(ipsr);
+
+      try (BufferedReader br =
+          new BufferedReader(new InputStreamReader(new FileInputStream(fileFacts), UTF_8))) {
+
         @Var
-        String ligne;
+        String row;
 
-        while ((ligne = br.readLine()) != null) {
-          if (ligne.charAt(0) != '%')
-            kbb.add(DlgpParser.parseAtom(ligne));
+        while ((row = br.readLine()) != null) {
+          if (row.charAt(0) != '%') {
+            kbb.add(DlgpParser.parseAtom(row));
+          }
         }
-
-        br.close();
-        ipsr.close();
-        ips.close();
-
       } catch (Exception e) {
-        System.out.println(e.toString());
+        Throwables.getRootCause(e).printStackTrace();
       }
     }
-
     return kbb;
   }
 
   public static String displayFacts(AtomSet facts) {
-    StringBuilder s = new StringBuilder("== Saturation ==\n");
-
-    try {
-      for (CloseableIterator<Atom> itAtom = facts.iterator(); itAtom.hasNext();) {
-        s.append(itAtom.next().toString());
-        s.append(".\n");
+    StringBuilder sb = new StringBuilder("== Saturation ==\n");
+    try (CloseableIterator<Atom> itAtom = facts.iterator()) {
+      while (itAtom.hasNext()) {
+        sb.append(itAtom.next().toString());
+        sb.append(".\n");
       }
     } catch (IteratorException e) {
-      e.printStackTrace();
+      Throwables.getRootCause(e).printStackTrace();
     }
-
-    return s.toString();
+    return sb.toString();
   }
 
   public static String getSaturationFromFile(String src,
       DefaultLabeledGraphOfRuleDependencies grd) {
+
     KBBuilder kbb = new KBBuilder();
     Utils.readKB(kbb, null, src);
-
     KnowledgeBase kb = kbb.build();
-
     SccChase<AtomSet> chase = new SccChase<>(grd, kb.getFacts());
+
     try {
       chase.execute();
     } catch (ChaseException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      Throwables.getRootCause(e).printStackTrace();
     }
-
     return Utils.displayFacts(kb.getFacts());
   }
 
   public static String getRulesText(Iterable<Rule> rules) {
-    StringBuilder s = new StringBuilder("====== RULE SET ======\n");
+    StringBuilder sb = new StringBuilder("====== RULE SET ======\n");
     for (Rule r : rules) {
-      s.append(r.toString());
-      s.append('\n');
+      sb.append(r.toString());
+      sb.append('\n');
     }
-    return s.toString();
+    return sb.toString();
   }
 
   public static String getGRDText(DefaultLabeledGraphOfRuleDependencies grd) {
-    StringBuilder s = new StringBuilder("======== GRD =========\n");
+    StringBuilder sb = new StringBuilder("======== GRD =========\n");
     for (Rule r1 : grd.getRules()) {
       for (Rule r2 : grd.getTriggeredRules(r1)) {
-        s.append("[");
-        s.append(r1.getLabel());
-        s.append("] ={+}=> [");
-        s.append(r2.getLabel());
-        s.append("]\n");
+        sb.append("[");
+        sb.append(r1.getLabel());
+        sb.append("] ={+}=> [");
+        sb.append(r2.getLabel());
+        sb.append("]\n");
       }
       for (Rule r2 : grd.getInhibitedRules(r1)) {
-        s.append("[");
-        s.append(r1.getLabel());
-        s.append("] ={-}=> [");
-        s.append(r2.getLabel());
-        s.append("]\n");
+        sb.append("[");
+        sb.append(r1.getLabel());
+        sb.append("] ={-}=> [");
+        sb.append(r2.getLabel());
+        sb.append("]\n");
       }
     }
-    return s.toString();
+    return sb.toString();
   }
 
   public static String getSCCText(StronglyConnectedComponentsGraph<Rule> scc) {
-    StringBuilder s = new StringBuilder("======== SCC =========\n");
+    StringBuilder sb = new StringBuilder("======== SCC =========\n");
     for (int i = 0; i < scc.getNbrComponents(); i++) {
       @Var
       boolean first = true;
-      s.append("C");
-      s.append(i);
-      s.append(" = {");
+      sb.append("C");
+      sb.append(i);
+      sb.append(" = {");
       for (Rule r : scc.getComponent(i)) {
-        if (first)
+        if (first) {
           first = false;
-        else
-          s.append(", ");
-        s.append(r.getLabel());
+        } else {
+          sb.append(", ");
+        }
+        sb.append(r.getLabel());
       }
-      s.append("}\n");
+      sb.append("}\n");
     }
-    return s.toString();
+    return sb.toString();
   }
 }
